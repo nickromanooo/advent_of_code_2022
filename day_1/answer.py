196703619f25564f40feeff29277439a1334703c
nick.romano <nick.romano@onlogic.com>
2023-12-01 11:31:25 -0500

    Fix double output

--- day_1/answer.py
+++ day_1/answer.py
@@ -15,5 +15,4 @@ def part_two():
     elf_sums = sorted([sum(elf) for elf in elves], reverse=True)
     return sum(elf_sums[0:3])
 
-print(f"Part one: {part_one()}")
 print(f"Part two: {part_two()}")
