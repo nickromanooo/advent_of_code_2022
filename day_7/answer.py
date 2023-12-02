7dee3c06feaa6b80d280f8a20e3425fb4f6a5abd
nick <nick.romano>
2023-12-02 00:00:54 -0500

    day 7

--- day_7/answer.py
+++ day_7/answer.py
@@ -1,19 +1,22 @@
-# https://adventofcode.com/2022/day/6
+# https://adventofcode.com/2022/day/7
 
 
 
 def part_one(file):
     f = open(file,'r')
 
-    lines = f.read().strip().split('$ ')
-    directory = []
+    commands = f.read().strip().split('$ ')
     directory_sub_totals = {}
     directory_structure = {}
     dir_stack = ['/']
-    for line in lines:
-        line = line.strip()
-        if line[:2] == 'cd':
-            new_dir = line[3:]
+    for com in commands:
+        com = com.strip()
+        if len(com) == 0:
+            continue
+
+        if com[:2] == 'cd':
+            _,dir = com.split()
+            new_dir = com[3:].strip()
             old_dir = dir_stack[-1]
             if new_dir == old_dir:
                 continue
@@ -21,44 +24,99 @@ def part_one(file):
             if new_dir == '..':
                 if old_dir == '/':
                     continue
-                dir_stack.pop(-1)
+                dir_stack.pop()
             else :
                 dir_stack.append(new_dir)
-
+            # print(f'dir stack updated {dir_stack}')
             continue
 
-        if line[:2] != 'ls':
-            continue
-        outputs = line.split('\n')[1:]
-        commands = [x.split(' ') for x in outputs]
-        sub_total = sum([int(y[0]) for y in commands if y[0].isdigit()])
-        sub_directories = [y[1] for y in commands if y[0] == 'dir']
-        directory_sub_totals[dir_stack[-1]] = sub_total
+        if com[:2] != 'ls':
+            raise Exception(f'weird line? -{com}')
+
+        outputs = com.split('\n')[1:]
+        blargle = [x.split(' ') for x in outputs]
+        sub_total = sum([int(y[0]) for y in blargle if y[0].isdigit()])
+        sub_directories = [y[1] for y in blargle if y[0] == 'dir']
+
+        directory_sub_totals['/'.join(dir_stack)] = sub_total
         if len(sub_directories) == 0:
-            directory_structure[dir_stack[-1]] = []
+            directory_structure['/'.join(dir_stack)] = []
         else:
-            directory_structure[dir_stack[-1]] = sub_directories
+            directory_structure['/'.join(dir_stack)] = ['/'.join(dir_stack + [x]) for x in sub_directories ]
 
     def get_directory_values(key):
+        if key not in directory_structure:
+            # print(f'key: {key}')
+            return 0
         sub_dirs = directory_structure[key]
         if len(sub_dirs) == 0:
             return directory_sub_totals[key]
-        
+
         return sum([get_directory_values(x) for x in sub_dirs]) + directory_sub_totals[key]
 
-    # print(directory_sub_totals)
-    print(len(directory_structure))
-    print(directory_structure.keys())
     selected_totals = [get_directory_values(d) for d in directory_structure.keys()]
     selected_totals = [x for x in selected_totals if x <= 100000]
-
-    print(f"/: {directory_structure['/']}")
-    for key in directory_structure['/']:
-        print(f'{key}: {directory_structure[key]}')
     return sum(selected_totals)
 
 
+def part_two(file):
+    f = open(file,'r')
+
+    commands = f.read().strip().split('$ ')
+    directory_sub_totals = {}
+    directory_structure = {}
+    dir_stack = ['/']
+    for com in commands:
+        com = com.strip()
+        if len(com) == 0:
+            continue
+
+        if com[:2] == 'cd':
+            _,dir = com.split()
+            new_dir = com[3:].strip()
+            old_dir = dir_stack[-1]
+            if new_dir == '/':
+                continue
+
+            if new_dir == '..':
+                if old_dir == '/':
+                    continue
+                dir_stack.pop()
+            else :
+                dir_stack.append(new_dir)
+            continue
+
+        if com[:2] != 'ls':
+            raise Exception(f'weird line? -{com}')
+
+        outputs = com.split('\n')[1:]
+        blargle = [x.split(' ') for x in outputs]
+        sub_total = sum([int(y[0]) for y in blargle if y[0].isdigit()])
+        sub_directories = [y[1] for y in blargle if y[0] == 'dir']
+
+        directory_sub_totals['/'.join(dir_stack)] = sub_total
+        if len(sub_directories) == 0:
+            directory_structure['/'.join(dir_stack)] = []
+        else:
+            directory_structure['/'.join(dir_stack)] = ['/'.join(dir_stack + [x]) for x in sub_directories ]
+
+    def get_directory_values(key):
+        if key not in directory_structure:
+            raise Exception(f'BAD KEY: {key}')
+            return 0
+        sub_dirs = directory_structure[key]
+        if len(sub_dirs) == 0:
+            return directory_sub_totals[key]
+
+        return sum([get_directory_values(x) for x in sub_dirs]) + directory_sub_totals[key]
+
+    selected_totals = [get_directory_values(d) for d in directory_structure.keys()]
+    max = 70000000
+    cur_filesystem_size = get_directory_values('/')
+    selected_totals = [x for x in selected_totals if (cur_filesystem_size - x) <= 40000000 ]
+    return sorted(selected_totals)[0]
+
 print(f"Part one test: {part_one('test_input.txt')}")
 print(f"Part one: {part_one('input.txt')}")
-# print(f"Part two test: {part_two('test_input.txt')}")
-# print(f"Part two: {part_two('input.txt')}") # 3263
+print(f"Part two test: {part_two('test_input.txt')}")
+print(f"Part two: {part_two('input.txt')}") # 8998590
